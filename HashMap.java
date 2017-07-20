3a93885d5e50ab9e9ce6777d343dd368f93b3063
memetibryan <memetibryan@gmail.com>
2017-07-20 13:04:25 +0300

    added HashMap code

---
+++ HashMap.java
@@ -0,0 +1,20 @@
+> import java.util.Map;
+Imported java.util.Map
+
+>  import java.util.HashMap;
+Imported java.util.HashMap
+
+> Map<Integer, String> numbersAndWords = new HashMap<Integer, String>();
+java.util.Map<java.lang.Integer, java.lang.String> numbersAndWords = {}
+
+> numbersAndWords.put(1, "one");
+java.lang.Object res13 = null
+
+> numbersAndWords.put(2, "two");
+java.lang.Object res14 = null
+
+> numbersAndWords.put(3, "three");
+java.lang.Object res15 = null
+
+> numbersAndWords;
+java.util.HashMap res16 = {1=one, 2=two, 3=three}
