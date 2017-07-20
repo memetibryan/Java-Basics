8835d0ff1d9ec191bd26d4d4531bc7fc96cb0fb8
memetibryan <memetibryan@gmail.com>
2017-07-20 13:15:04 +0300

    added HashMap explanation code

--- HashMap.java
+++ HashMap.java
@@ -19,4 +19,54 @@ java.lang.Object res15 = null
 > numbersAndWords;
 java.util.HashMap res16 = {1=one, 2=two, 3=three}
 
-//use .get(value); -to retrieve Hash Info
+//to retrieve Hash Info
+==================
+//.get(value);
+-----------------------------------------------------
+
+
+// Returning all Keys
+======================
+// words.keySet();
+// java.util.HashMap.keySet res = [fish, shoes, rain]
+=========================================================
+// This returns a key set that can be used in a for each loop.
+-------------------------------------------------------------------
+
+//Returning All Values
+======================
+//numbers.values()
+//java.util.HashMap.Values res = [503-555-1212, 415-999-0000, 202-111-5599]
+==================================================================
+// This returns all values in the hash map saved into an array.
+------------------------------------------------------------------------
+
+//Checking if a HashMap Contains a Specific Key
+==============================================
+//words.containsKey("fish");
+//java.lang.boolean res9 = true
+==================================================
+// This returns true if the hash map contains the key.
+-----------------------------------------------------------------
+
+//Checking if a HashMap Contains a Specific Value
+=====================================================
+//> capitols.containsValue("123-456-7890");
+//java.lang.boolean res10 = false
+===================================================
+// This returns true if hash map contains the value.
+-------------------------------------------------------------
+
+//Iterating Through HashMap (We can iterate through a hash map by using the .keySet(); method. )
+=================================================================================================
+// for ( Object key : numbers.keySet() ) {
+//   System.out.println(numbers.get(key));
+// }
+-------------------------------------------------------------
+
+//or we can use the .values() method:
+================================================== 
+// for ( Object value : words.values() ) {
+//   System.out.println(value);
+// }
+----------------------------------------------------
